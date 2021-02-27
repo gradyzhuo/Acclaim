efc8a7225ad8f4ae227d20cdf8753381559a4670
Grady Zhuo <gradyzhuo@gmail.com>
2021-02-28 01:53:16 +0800

    @updated Packages

--- Package.swift
+++ Package.swift
@@ -15,6 +15,10 @@ let package = Package(
             name: "Procedure",
             targets: ["Procedure"]
         ),
+        .library(
+            name: "Logger",
+            targets: ["Logger"]
+        ),
         .executable(
             name: "LoggerDemo",
             targets: ["Demo"]
@@ -22,8 +26,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        // .package(url: /* package url */, from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -39,7 +42,6 @@ let package = Package(
         .target(
             name: "Logger",
             dependencies: [
-                .product(name: "Logging", package: "swift-log"),
                 .target(name: "ShellHelper")
             ]
         ),
